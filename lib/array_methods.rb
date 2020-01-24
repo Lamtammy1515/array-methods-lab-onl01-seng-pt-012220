c649038e1a96cc80825973a3090c47659d05ac1e
Tammy Lam <lamtammy1515@gmail.com>
2020-01-24 18:48:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("wow") 
-  array.include?("Tardis")
+  if array.include?("wow") 
+   puts true
+  elsif array.include?("Tardis")
+  puts false
   
 end
 
